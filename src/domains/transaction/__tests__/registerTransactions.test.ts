ecdf5af070880fb0eb8295558ae422db9b62baf8
nandapieri <nandapieri@gmail.com>
2025-02-10 16:55:47 -0300

    feat: create register transaction test

---
+++ src/domains/transaction/__tests__/registerTransactions.test.ts
@@ -0,0 +1,62 @@
+import { TransactionsDatabase } from "../database/types";
+import registerTransaction from "../registerTransaction";
+
+
+const mockDatabase: TransactionsDatabase = {
+  saveTransaction: jest.fn(),
+  listTransactions: jest.fn(),
+  calculateMonthlyBalance: jest.fn(),
+};
+
+describe("registerTransaction", () => {
+  const userId = "user-123";
+  const amount = 100;
+  const description = "Test transaction";
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should register a transaction successfully", async () => {
+    const register = registerTransaction(mockDatabase);
+
+    const transaction = await register(userId, amount, description);
+
+    expect(mockDatabase.saveTransaction).toHaveBeenCalledTimes(1);
+    expect(mockDatabase.saveTransaction).toHaveBeenCalledWith(transaction);
+
+    expect(transaction).toEqual({
+      id: expect.any(String),
+      userId,
+      amount,
+      createdAt: expect.any(String),
+      description,
+    });
+  });
+
+  it("should throw an error if amount is zero", async () => {
+    const register = registerTransaction(mockDatabase);
+
+    await expect(register(userId, 0, description)).rejects.toThrow("Amount must not be zero.");
+    expect(mockDatabase.saveTransaction).not.toHaveBeenCalled();
+  });
+
+  it("should throw an error if description is empty", async () => {
+    const register = registerTransaction(mockDatabase);
+
+    await expect(register(userId, amount, "")).rejects.toThrow("Description is required.");
+    expect(mockDatabase.saveTransaction).not.toHaveBeenCalled();
+  });
+
+  it("should throw an error if saving transaction fails", async () => {
+    const mockDatabase: TransactionsDatabase = {
+        saveTransaction: jest.fn().mockRejectedValueOnce(new Error("Save failed")),
+        listTransactions: jest.fn(),
+        calculateMonthlyBalance: jest.fn(),
+      };
+    const register = registerTransaction(mockDatabase);
+
+    await expect(register(userId, amount, description)).rejects.toThrow("Could not register transaction.");
+    expect(mockDatabase.saveTransaction).toHaveBeenCalledTimes(1);
+  });
+});
